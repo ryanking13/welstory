59fed32573b7c63751de5c87923daaad8e8545e8
sihadan <def6488@gmail.com>
2019-09-05 16:56:09 +0900

    reformat codes

---
+++ course.py
@@ -0,0 +1,44 @@
+class Menu:
+    def __init__(self, menu):
+        self.course = menu["course_txt"]
+        self.name = menu["menu_name"]
+        self.image = (
+            menu["image_url"] + menu["photo_cd"]
+            if menu["image_url"] is not None
+            else ""
+        )
+        self.kcal = menu["kcal"]
+
+    def __repr__(self):
+        return f"{self.name}({self.kcal} kcal)"
+
+    def dump(self):
+        return {"name": self.name, "image": self.image, "kcal": self.kcal}
+
+
+class Course:
+    def __init__(self):
+        self.kcal = 0
+        self.main = None
+        self.side = []
+
+    def add_menu(self, menu):
+        m = Menu(menu)
+        if menu["tot_kcal"] != 0:  # only main dish contains tot_kcal value
+            self.kcal = menu["tot_kcal"]
+            self.main = m
+        else:
+            self.side.append(m)
+
+    def __repr__(self):
+        return f"{repr(self.main)}/" + "/".join([repr(s) for s in self.side])
+
+    def dump(self):
+        d = {
+            "kcal": self.kcal,
+            "course": self.main.course,
+            "main": self.main.dump(),
+            "side": [s.dump() for s in self.side],
+        }
+
+        return d
---
+++ get_menu.py
@@ -0,0 +1,79 @@
+from collections import defaultdict
+import datetime
+import requests
+import json
+from course import Course
+
+
+class MealType:
+    LUNCH = 2
+    DINNER = 3
+
+
+class RestaurantCode:
+    # TODO: add more codes
+    MULTICAMPUS = "REST000133"
+
+
+LOGIN_URL = "http://mw.welstory.com/nsimple_member_insert.do"
+MENU_URL = "http://mw.welstory.com/nmenu_today.do"
+
+
+def today():
+    # YYYYMMDD format
+    return str(datetime.datetime.today().strftime("%Y%m%d"))
+
+
+def login():
+    sess = requests.session()
+    sess.post(
+        LOGIN_URL,
+        data={
+            "MODULE": "LOGIN_SIMPLE",
+            "mem_name": "김이박",
+            "phone": "01012341234",
+            "birth": "000000",
+            "sex": "3",
+            "agree_date": today(),
+            "agree_event": "0",
+            "device_type": "I",
+            "app_type": "1001",
+        },
+    )
+
+    return sess
+
+
+def get_menu(code=RestaurantCode.MULTICAMPUS, date=None, meal_type=MealType.LUNCH):
+    if date is None:
+        date = today()
+
+    data = {
+        "MODULE": "TODAY_MENU_LIST",
+        "app_type": "1001",
+        "device_type": "I",
+        "restaurant_code": code,
+        "date": date,
+        "meal_type": meal_type,
+    }
+
+    sess = login()
+    resp = sess.post(MENU_URL, data=data, allow_redirects=False)
+
+    # if authentication failed, status_code will be 302(redirect)
+    if resp.status_code != 200:
+        raise (ConnectionError("Authentication failed, maybe cookie expired"))
+
+    menus = resp.json()["menu_array"]
+
+    courses = defaultdict(lambda: Course())
+    for menu in menus:
+        course = menu["course_txt"]
+        courses[course].add_menu(menu)
+
+    courses_dump = [c.dump() for c in courses.values()]
+    return courses_dump
+
+
+if __name__ == "__main__":
+    print(get_menu(date=today()))
