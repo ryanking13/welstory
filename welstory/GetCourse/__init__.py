49b6125bdd503d06938d7499bb2925ce7dc0ce03
sihadan <def6488@gmail.com>
2019-09-09 17:03:17 +0900

    fix directory structure

--- welstory/GetCourse/__init__.py
+++ welstory/GetCourse/__init__.py
@@ -1,24 +1,41 @@
 import logging
-
+from welstory import get_menu, RestaurantCode, MealType
 import azure.functions as func
+
+DEFAULT_CODE = RestaurantCode.MULTICAMPUS
+DEFAULT_MEAL_TYPE = MealType.LUNCH
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("[GetCourse] processed a request.")
 
-    name = req.params.get('name')
-    if not name:
+    code = req.params.get("code")
+    if not code:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            code = req_body.get("code")
 
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
-    else:
-        return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
-        )
+    meal_type = req.params.get("meal_type")
+    if not meal_type:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            meal_type = req_body.get("meal_type")
+
+    if not code:
+        code = DEFAULT_CODE
+    if not meal_type:
+        meal_type = DEFAULT_MEAL_TYPE
+
+    # if CODE:
+    #     return func.HttpResponse(f"Hello {name}!")
+    # else:
+    #     return func.HttpResponse(
+    #         "Please pass a name on the query string or in the request body",
+    #         status_code=400,
+    #     )
