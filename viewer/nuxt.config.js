b06caf8caaa7cedc6a67876bb4e1452d4568bc6a
sihadan <def6488@gmail.com>
2019-09-16 11:20:56 +0900

    add configurations for github page and analytics

--- viewer/nuxt.config.js
+++ viewer/nuxt.config.js
@@ -24,7 +24,7 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
@@ -34,6 +34,15 @@ module.exports = {
         })
       }
     }
-  }
+  },
+  generate: {
+    dir: '../docs',
+  },
+  router: {
+    base: '/welstory/'
+  },
+  plugins: [
+    { src: '~plugins/ga.js', ssr: false }
+  ]
 }
 
